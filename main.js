0ddb30fe29427be8c94fbb4b7110f598fbe6f356
Artur Siqueira <artursiqueirasouto@hotmail.com>
2023-02-15 20:47:32 -0300

    Melhorando navegação por teclado com condições

--- main.js
+++ main.js
@@ -20,12 +20,23 @@ while( contador < listaDeTeclas.length) {
 */
 
 for(let i = 0; i < listaDeTeclas.length; i++){
-    
+
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
     listaDeTeclas[i].onclick = function() {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
     }
 }
